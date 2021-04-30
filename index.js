4d831dc4bfbe2d7c5872a50954b26414c9ace575
orchardstreet <39713147+orchardstreet@users.noreply.github.com>
2021-04-30 06:07:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { abi1, abi2, address1, address2, url } = require('./blockchaincredentials.js');
+const { abi1, abi2, address1, address2, url, url2, abi3, abi4, address3, address4 } = require('./blockchaincredentials.js');
 var Web3 = require('web3');
 var web3 = new Web3(url);
+var web3_matic = new Web3(url2);
 var contract1 = new web3.eth.Contract(abi1, address1);
 var contract2 = new web3.eth.Contract(abi2, address2);
-
+var contract3 = new web3_matic.eth.Contract(abi3, address3);
+var contract4 = new web3_matic.eth.Contract(abi4, address4);
+//console.log(web3_matic)
 var pixels = 0;
 
 function getRemoteTokenTransactionDataForId(tokenId) {
@@ -23,20 +26,106 @@ function getRemoteTokenTransactionDataForId(tokenId) {
                     resolve(events[0].returnValues)
                 }
             })
+    }).catch(function(err) {
+        console.log("aaa", err);
+        //  throw err;
+
+    });
+}
+
+function failureCallback(error) {
+    console.error("Error generating audio file: " + error);
+}
+
+function getRemoteTokenTransactionDataForId_matic(tokenId, fromm, tooo) {
+    return new Promise(async function(resolve, reject) {
+        contract4.getPastEvents('Painted', {
+            filter: {
+                tokenId: web3_matic.utils.toBN(tokenId)
+            },
+            fromBlock: fromm,
+            toBlock: tooo
+        }, function(err, events) {
+            if (err) {
+                console.log("new request");
+                reject(err);
+            } else {
+                console.log("new request");
+                if (events.length == 0) {
+                    resolve('nonefound');
+                } else {
+                    resolve(events[0].returnValues);
+                }
+            }
+
+        })
     })
+
 }
 
 async function mainn() {
     var latestBlockchainID = await contract1.methods.totalSupply().call();
     for (var i = 0; i <= latestBlockchainID - 1; i++) {
         var temp = await getRemoteTokenTransactionDataForId(i);
-        console.log(i + "th NFT");
+        console.log(i + "th Ethereum NFT");
         process(temp.transparentPixelGroups, temp.pixelGroups, temp.pixelData);
     }
-    console.log("NUMBER OF PIXELS: " + pixels);
+    var ethpixels = pixels;
+    console.log("NUMBER OF PIXELS on ETH: " + ethpixels);
     console.log("NUMBER OF NFTs PROCESSED FROM ETH: " + (latestBlockchainID - 1));
-    console.log("AMOUNT OF $PAINT BURNED ON ETH: " + pixels / 2);
-    
+    var ethtotal = pixels / 2;
+    console.log("AMOUNT OF $PAINT BURNED ON ETH: " + ethtotal);
+
+    pixels = 0;
+    var latestBlockchainID_matic = await contract3.methods.totalSupply().call().catch(function(err) {
+        latestBlockchainID_matic--;
+        console.log(err);
+    });
+    var firstBlock = 13114876;
+    var i = 0;
+    var x = 0;
+    var blockstep = 4000;
+    var lastblock = firstBlock + blockstep;
+    console.log(latestBlockchainID_matic);
+    while (i < latestBlockchainID_matic && firstBlock < 13885818) {
+        x++;
+        console.log("searching in same block range: " + x + " times");
+        var temp2 = await getRemoteTokenTransactionDataForId_matic(i, firstBlock, lastblock).catch(function(err) {
+            console.log(err)
+        });
+        if (temp2 == "nonefound") {
+            console.log("\n\n\n\n\n\nNONE FOUND\n\n\n\n\n");
+            firstBlock = firstBlock + blockstep;
+            lastblock = firstBlock + blockstep;
+            console.log("adjusting");
+            console.log(firstBlock);
+            console.log(lastblock);
+        } else {
+            //console.log(temp2);
+            process(temp2.transparentPixelGroups, temp2.pixelGroups, temp2.pixelData);
+            console.log("found and got data for NFT #: " + i);
+            i = i + 1;
+        }
+
+    }
+    var maticpixels = pixels;
+    var matictotal = pixels / 2;
+
+    console.log(temp2);
+    //var temp2 = await test(i);
+    //    var okk = await contract4.getPastEvents('Painted');
+    //		process(temp2.transparentPixelGroups,temp2.pixelGroups,temp2.pixelData);
+    console.log("NUMBER OF PIXELS on ETH: " + ethpixels);
+    console.log("NUMBER OF PIXELS on MATIC: " + maticpixels);
+    var pixeltotal = ethpixels + maticpixels;
+    console.log("TOTAL NUMBER OF PIXELS: " + pixeltotal);
+    console.log("NUMBER OF NFTs PROCESSED FROM ETH: " + (latestBlockchainID - 1));
+    console.log("NUMBER OF NFTs PROCESSED FROM MATIC: " + (latestBlockchainID_matic - 1));
+    console.log("\n\n\n");
+    console.log("AMOUNT OF $PAINT BURNED ON ETH: " + ethtotal);
+    console.log("AMOUNT OF $PAINT BURNED ON MATIC: " + matictotal);
+    var completetotal = ethtotal + matictotal;
+    console.log("\n\n\nTOTAL NUMBER OF PIXELS BURNED: " + completetotal);
 }
 
 
@@ -50,6 +139,8 @@ function process(decimalTransparentPixelGroups, decimalPixelGroups, decimalPixel
 
     //beginning of pixelData
     if (decimalPixelData.length != 0) {
+
+        //convert decimalPixelData to hex, and leftpad them
         for (var x = 0; x < decimalPixelData.length; x++) {
             hexPixelData.push(decToHex(decimalPixelData[x]));
             hexPixelData[x] = hexPixelData[x].slice(2);
@@ -60,30 +151,30 @@ function process(decimalTransparentPixelGroups, decimalPixelGroups, decimalPixel
 
     //beginning of pixel groups
     if (decimalPixelGroups.length != 0) {
-            pixels += decimalPixelGroups.length * 32;
+        pixels += decimalPixelGroups.length * 32;
     }
 
 
     //beginning of hexTransparentPixelGroups
-    if (decimalTransparentPixelGroups.length != 0 ) {
+    if (decimalTransparentPixelGroups.length != 0) {
 
         for (var x = 0; x < decimalTransparentPixelGroups.length; x++) {
             hexTransparentPixelGroups.push(decToHex(decimalTransparentPixelGroups[x]));
             hexTransparentPixelGroups[x] = hexTransparentPixelGroups[x].slice(2);
-           if (hexTransparentPixelGroups[x].length % 2 != 0) {
-                    hexTransparentPixelGroups[x] = "0" + hexTransparentPixelGroups[x];
+            if (hexTransparentPixelGroups[x].length % 2 != 0) {
+                hexTransparentPixelGroups[x] = "0" + hexTransparentPixelGroups[x];
             }
             for (var y = 0; y < hexTransparentPixelGroups[x].length; y = y + 2) {
-                    if (hexTransparentPixelGroups[x].slice(y,y+2) == "00")
-                    {
-            //              console.log("hex value of transparenPixelGroups: it's 00");
-                    } else {
+                if (hexTransparentPixelGroups[x].slice(y, y + 2) == "00") {
+                    //		console.log("hex value of transparenPixelGroups: it's 00");
+                } else {
                     pixels++;
-                   }
+                }
             }
         }
-    //closing tag forr transparentpixelgroups
+        //closing tag forr transparentpixelgroups
     }
+
     console.log("finished processing");
 }
 
@@ -175,4 +266,5 @@ function decToHex(decStr) {
     var hex = convertBase(decStr, 10, 16);
     return hex ? '0x' + hex : null;
 }
+
 //END OF DECTOHEX
